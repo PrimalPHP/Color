9b72e72b5da96280071429b3ec52c998441d0a49
Vitaly Chirkov <vitaly.chirkov@gmail.com>
2015-10-29 00:50:25 +0300

    Fix some conversion errors

--- lib/Primal/Color/HSLColor.php
+++ lib/Primal/Color/HSLColor.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 namespace Primal\Color;
 
@@ -7,45 +7,45 @@ class HSLColor extends Color {
 	public $saturation = 0;
 	public $luminance = 0;
 	public $alpha = 1;
-	
+
 	function __construct($h = 0, $s = 0, $l = 0, $a=1) {
 		$this->hue        = $h;
 		$this->saturation = $s;
 		$this->luminance  = $l;
 		$this->alpha      = $a;
 	}
-	
+
 	public function toHSV() {
 		return $this->toRGB()->toHSV();
 	}
-	
+
 	public function toHSL() {
 		return clone $this;
 	}
-	
+
 	function toCMYK() {
 		return $this->toRGB()->toCMYK();
 	}
-	
+
 	public function toRGB() {
 		$h = ($this->hue        % 360) / 360;
 		$s = ($this->saturation % 101) / 100;
 		$l = ($this->luminance  % 101) / 100;
-		$a = $this->alpha;
-		
+
 		if ($s === 0) {
-			return new RGBColor($l * 255, $l * 255, $l * 255);
+			return new RGBColor($l * 255, $l * 255, $l * 255, $this->alpha);
 		} else {
-			$q = ($l < 0.5) ? $l * (1 + $s) : $l + $s - $l * $s;
+			$q = $l < 0.5 ? $l * (1 + $s) : $l + $s - $l * $s;
 			$p = 2 * $l - $q;
 			return new RGBColor(
 				static::HueToFactor($p, $q, $h + 1 / 3) * 255,
 				static::HueToFactor($p, $q, $h        ) * 255,
-				static::HueToFactor($p, $q, $h - 1 / 3) * 255
+				static::HueToFactor($p, $q, $h - 1 / 3) * 255,
+				$this->alpha
 			);
 		}
 	}
-	
+
 	protected static function HueToFactor($p, $q, $t) {
 		if ($t < 0) $t += 1;
 		if ($t > 1) $t -= 1;
@@ -54,9 +54,11 @@ class HSLColor extends Color {
 		if ($t < 2 / 3) return $p + ($q - $p) * (2/3 - $t) * 6;
 		return $p;
 	}
-	
+
 	public function toCSS($alpha = null) {
-		return (($alpha === true || $this->alpha < 1) && $alpha !== false) ? "hsla({$this->hue}, {$this->saturation}, {$this->luminance}, {$this->alpha})" : "hsl({$this->hue}, {$this->saturation}, {$this->luminance})";
+		return ($alpha === true || $this->alpha < 1) && $alpha !== false
+			? sprintf('hsla(%d, %d, %d, %s)', $this->hue, $this->saturation, $this->luminance, $this->alpha)
+			: sprintf('hsl(%d, %d, %d)', $this->hue, $this->saturation, $this->luminance);
 	}
 }
 
--- lib/Primal/Color/HSVColor.php
+++ lib/Primal/Color/HSVColor.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 namespace Primal\Color;
 
@@ -7,34 +7,34 @@ class HSVColor extends Color {
 	public $saturation = 0;
 	public $value = 0;
 	public $alpha = 1;
-	
+
 	function __construct($h = 0, $s = 0, $v = 0, $a=1) {
 		$this->hue        = $h;
 		$this->saturation = $s;
 		$this->value      = $v;
 		$this->alpha      = $a;
 	}
-	
+
 	function toHSV() {
 		return clone $this;
 	}
-	
+
 	function toHSL() {
 		return $this->toRGB()->toHSL();
 	}
-	
+
 	function toCMYK() {
 		return $this->toRGB()->toCMYK();
 	}
-	
+
 	function toRGB() {
 		$h = ($this->hue        % 360) / 360;
 		$s = ($this->saturation % 101) / 100;
 		$v = ($this->value      % 101) / 100;
 		$a = $this->alpha;
-		
+
 		$i = $h * 6;
-		$f = $h * 6 - $i;
+		$f = $i - floor($i);
 		$p = $v * (1 - $s);
 		$q = $v * (1 - $f * $s);
 		$t = $v * (1 - (1 - $f) * $s);
@@ -72,10 +72,10 @@ class HSVColor extends Color {
 		}
 		return new RGBColor($r * 255, $g * 255, $b * 255, $a);
 	}
-	
+
 	function toCSS($alpha = null) {
-		return (($alpha === true || $this->alpha < 1) && $alpha !== false) ? "hsla({$this->hue}, {$this->saturation}, {$this->value}, {$this->alpha})" : "hsl({$this->hue}, {$this->saturation}, {$this->value})";
+		return $this->toRGB()->toCSS($alpha);
 	}
-	
+
 }
 
