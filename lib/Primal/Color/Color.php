58b1f9da942b4083ec323792720bbaa2c4fe93a9
Sergey <FinesseRus@users.noreply.github.com>
2015-11-03 15:28:18 +0900

    Update Color.php

--- lib/Primal/Color/Color.php
+++ lib/Primal/Color/Color.php
@@ -4,14 +4,29 @@ namespace Primal\Color;
 
 abstract class Color {
 	
+	/**
+	 * @return HSVColor
+	 */
 	abstract function toHSV();
 	
+	/**
+	 * @return HLSColor
+	 */
 	abstract function toHSL();
 	
+	/**
+	 * @return RGBColor
+	 */
 	abstract function toRGB();
 	
+	/**
+	 * @return CMYKColor
+	 */
 	abstract function toCMYK();
 	
+	/**
+	 * @return string
+	 */
 	abstract function toCSS();
 }
 
